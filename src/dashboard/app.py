01f4dd69fdc861c62ec134999002b6c3fc211075
Hashmeet Randhawa <hashmeetrandhawa08@gmail.com>
2026-07-10 11:58:33 +0530

    app

---
+++ src/dashboard/app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Project VAYU",
+    page_icon="🌍",
+    layout="wide"
+)
+
+st.title("🌍 Project VAYU")
+
+st.subheader("AI Powered Urban Air Quality Intelligence Platform")
+
+st.write("---")
+
+st.write("Welcome to Project VAYU")
+
+st.write("""
+This platform predicts Air Quality Index (AQI),
+analyzes pollution,
+and provides AI-powered environmental insights.
+""")
